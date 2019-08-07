615b9cbce803abb0d58002963f59255ace3a759e
Spencer Rose <spencerkrose@gmail.com>
2019-08-07 14:27:16 -0500

    heroku

---
+++ client/build/precache-manifest.0ac6069c2b6955c34748272ebea30590.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ae93cc23d6348515ae9ae1fce9aa8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "932321caece127604a90",
+    "url": "/static/css/main.81db8d30.chunk.css"
+  },
+  {
+    "revision": "95d348436fe508936cd6",
+    "url": "/static/js/2.e25d7fc8.chunk.js"
+  },
+  {
+    "revision": "932321caece127604a90",
+    "url": "/static/js/main.a318dc6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
