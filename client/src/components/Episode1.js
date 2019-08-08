66f43184c8e248a2242cc9f677d5b8eaa09a671e
Spencer Rose <spencerkrose@gmail.com>
2019-08-08 13:28:10 -0500

    episode one page done

--- client/src/components/Episode1.js
+++ client/src/components/Episode1.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "../style.css"
-import PostIt from './PostIt'
 import Footer from "./Footer"
+import Plyr from "plyr"
+const player = new Plyr('#player');
 
 function Episode1() {
-    return (
-      <div>
-     <div className="center-align">
-        <img className="center-align container" src="https://i.imgur.com/RciEJS6.jpg"></img>
+  return (
+    <div>
+      <div className="center-align">
+      <img className="podcast" src="https://i.imgur.com/jqitUs2.png"></img>
+      <div className="podcast">
+      <audio className="podcast" id="player" controls>
+          <source src="https://deathatabakesale.s3.ca-central-1.amazonaws.com/Episode+1-+Lemon+Bars+with+Alex+and+Andrew+.mp3" type="audio/mp3" />
+          <source src="https://deathatabakesale.s3.ca-central-1.amazonaws.com/Episode+1-+Lemon+Bars+with+Alex+and+Andrew+.mp3" type="audio/ogg" />
+        </audio>
         </div>
-        <div>
-          <PostIt id="postIt"/>
-        </div>
-         <Footer/>
+        <img className="clueBoard center-align container" src="https://i.imgur.com/9WTxlV6.jpg"></img>
       </div>
-      
-    );
-  }
-  
-  export default Episode1;
+     
+
+
+      <div>
+       
+      </div>
+      <Footer />
+    </div>
+
+  );
+}
+
+export default Episode1;
