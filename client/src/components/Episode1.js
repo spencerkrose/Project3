5eaa117cc66ceb8d027890d6cf63d1d4bb5e067f
Spencer Rose <spencerkrose@gmail.com>
2019-08-08 10:47:48 -0500

    CSS fix

--- client/src/components/Episode1.js
+++ client/src/components/Episode1.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../style.css"
+import PostIt from './PostIt'
 import Footer from "./Footer"
 
 function Episode1() {
     return (
       <div>
-     
+     <div className="center-align">
+        <img className="center-align container" src="https://i.imgur.com/RciEJS6.jpg"></img>
+        </div>
+        <div>
+          <PostIt id="postIt"/>
+        </div>
          <Footer/>
       </div>
       
